957078e4f4f1e484e2c7e100c095c5762f1820c0
Shital <shital31>
2019-11-14 13:14:37 +0530

    practice examples

--- Basic-app/src/app/first/first.module.ts
+++ Basic-app/src/app/first/first.module.ts
@@ -1,15 +1,12 @@
-import { FirstSampleComponent } from './../first-sample/first-sample.component';
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { FirstSampleComponent } from "./../first-sample/first-sample.component";
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { MyCardComponent } from "./my-card/my-card.component";
 //import { FirstSampleComponent } from '../first-sample/first-sample.component';
 
-
-
 @NgModule({
-  declarations: [FirstSampleComponent],
-  imports: [
-    CommonModule
-  ],
-  exports : [FirstSampleComponent]
+  declarations: [FirstSampleComponent, MyCardComponent],
+  imports: [CommonModule],
+  exports: [FirstSampleComponent, MyCardComponent]
 })
-export class FirstModule { }
+export class FirstModule {}
