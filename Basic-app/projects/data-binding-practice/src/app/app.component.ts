1d5c8d8f5b7a48bc4ae5d954723a35f57e53b2ed
Shital <shital31>
2019-12-03 21:14:46 +0530

    practice example

--- Basic-app/projects/data-binding-practice/src/app/app.component.ts
+++ Basic-app/projects/data-binding-practice/src/app/app.component.ts
@@ -7,4 +7,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'data-binding-practice';
+  type = 'text';
+  type1 = 'button';
+
+  watsDate(){
+    return Date.now()
+  }
+
+  border= '1px solid red'
+
+  bxbkcl = 'white'
+
+  onMouseEnter(event:MouseEvent){
+    console.log('Entered',event)
+    this.bxbkcl= this.randomColor()
+  }
+  onMouseLeave(event : MouseEvent){
+    console.log('Exited',event)
+    this.bxbkcl = 'white'
+  }
+  // randomColor(): string {
+  //   let r: number = Math.floor(Math.random() * 200)
+  //   let g: number = Math.floor(Math.random() * 150)
+  //   let b: number = Math.floor(Math.random() * 255)
+  //   return(`rgb ${r},${g},${b}`)
+  //}
+  randomColor(): string {
+    let r: number = Math.floor(Math.random() * 255);
+    let g: number = Math.floor(Math.random() * 255);
+    let b: number = Math.floor(Math.random() * 255);
+    return `rgb(${r},${g},${b})`;
+  }
+
+
 }
