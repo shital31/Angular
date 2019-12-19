fb223756401b82df8bf91332768285e81326040b
Shital <shital31>
2019-12-19 16:49:16 +0530

    service part 1

--- Basic-app/projects/services/src/app/app.module.ts
+++ Basic-app/projects/services/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { ProgressService } from './progress.service';
 import { SampleService } from './sample.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
@@ -5,17 +6,21 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { DataLoggerComponent } from './data-logger/data-logger.component';
 import { DataMapperComponent } from './data-mapper/data-mapper.component';
+import { BarComponent } from './bar/bar.component';
+import { OpsComponent } from './ops/ops.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DataLoggerComponent,
-    DataMapperComponent
+    DataMapperComponent,
+    BarComponent,
+    OpsComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [SampleService],
+  providers: [SampleService,ProgressService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ Basic-app/projects/services/src/app/progress.service.ts
@@ -0,0 +1,5 @@
+
+export class ProgressService {
+
+  constructor() { }
+}
